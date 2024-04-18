7e01f3ec05ce97afe9060fb20e695949e557e2ad
Daniil Bobrov <allodrmen@yandex.ru>
2024-04-18 22:53:47 +0300

    remove useless files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='*' element={<NotFound />}/>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
