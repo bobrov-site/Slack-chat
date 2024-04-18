a32e0a3ec4c0b20c0d26df1fc3a586c7f554fabc
Daniil Bobrov <allodrmen@yandex.ru>
2024-04-18 22:03:33 +0300

    imported styles to bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <BrowserRouter>
