1621e5ea9149e30a25ebb8bb334e32d84372ed42
Daniil Bobrov <allodrmen@yandex.ru>
2024-04-22 22:11:41 +0300

    added login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,37 +1,60 @@
+import axios from 'axios';
 import { Formik, Form, Field } from 'formik';
+import routes from '../routes';
+import { useNavigate } from 'react-router-dom';
 // import Form from 'react-bootstrap/Form';
-const nicknameId = 'nickname';
-const passwordId = 'password';
-const Login = () => (
-  <Formik
-    initialValues={{ nickname: '', password: '' }}
-    onSubmit={({ setSubmitting }) => {
-      console.log('Form is validated! Submitting the form...');
-      setSubmitting(false);
-    }}
-  >
-    <Form>
-      <div className="form-group">
-        <label htmlFor={nicknameId}>Никнейм</label>
-        <Field
-          type="text"
-          name="nickname"
-          className="form-control"
-          id={nicknameId}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor={passwordId}>Пароль</label>
-        <Field
-          type="password"
-          name="password"
-          className="form-control"
-          id={passwordId}
-        />
-      </div>
-      <button type="submit">Войти</button>
-    </Form>
-  </Formik>
-);
+const axiosConfig = {
+  headers: {
+    "Content-Type" : 'application/json',
+    "Accept": "application/json"
+  }
+};
+const Login = () => {
+  const navigate = useNavigate();
+  const nicknameId = 'nickname';
+  const passwordId = 'password';
+  return (
+    (
+      <Formik
+        initialValues={{ nickname: '', password: '' }}
+        onSubmit={async (values, { setSubmitting }) => {
+          const { nickname, password } = values;
+          setSubmitting(false);
+          try {
+            const response = await axios.post(routes.login(), { username: nickname, password: password }, axiosConfig );
+            // response.data.token response.data.user;
+            localStorage.setItem('token', response.data.token);
+            return navigate('/');
+          }
+          catch (e) {
+            const { message } = e.response.data;
+          }
+        }}
+      >
+        <Form>
+          <div className="form-group">
+            <label htmlFor={nicknameId}>Никнейм</label>
+            <Field
+              type="text"
+              name="nickname"
+              className="form-control"
+              id={nicknameId}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor={passwordId}>Пароль</label>
+            <Field
+              type="password"
+              name="password"
+              className="form-control"
+              id={passwordId}
+            />
+          </div>
+          <button type="submit">Войти</button>
+        </Form>
+      </Formik>
+    )
+  )
+};
 
 export default Login;
