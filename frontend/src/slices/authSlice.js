25d078198cc02508fd372eefaefc04e43e3b8557
Daniil Bobrov <allodrmen@yandex.ru>
2024-04-25 22:29:26 +0300

    created authSlice

---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    token: localStorage.getItem('token') ?? null
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setUserData: (state, action) => {
+            state.token = action.payload;
+        }
+    }
+});
+
+export const { setUserData } = authSlice.actions;
+export default authSlice.reducer;
