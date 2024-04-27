0b5e2517f14e09915044b8f667055a223a6fd90f
Daniil Bobrov <allodrmen@yandex.ru>
2024-04-27 12:32:07 +0300

    check token when go to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,28 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
-const Home = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/one">Page One</Link>
-      </li>
-      <li>
-        <Link to="/two">Page Two</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Home = () => {
+  const { token } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!token) {
+      return navigate('/login');
+    }
+  }, [token, navigate]);
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/one">Page One</Link>
+        </li>
+        <li>
+          <Link to="/two">Page Two</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Home;
