59a2b7b9a456e067d619dc58d3bbb6041bd5ae92
Daniil Bobrov <allodrmen@yandex.ru>
2024-04-27 12:22:14 +0300

    added provider to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { rootReducer } from './slices';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<NotFound />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  const store = configureStore({
+    reducer: rootReducer,
+  });
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 export default App;
