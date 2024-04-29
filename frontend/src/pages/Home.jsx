d3b9b888708427fee59140e709d2c6bc0e721467
Daniil Bobrov <allodrmen@yandex.ru>
2024-04-29 20:54:34 +0300

    remove layout of channels

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,11 +5,11 @@ import Button from 'react-bootstrap/esm/Button';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { Plus, Send } from 'react-bootstrap-icons';
-import Nav from 'react-bootstrap/Nav';
+import { Send } from 'react-bootstrap-icons';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import { Formik } from 'formik';
+import Channels from '../components/channels/Channels';
 const Home = () => {
   const { token } = useSelector((state) => state.auth);
   const navigate = useNavigate();
@@ -21,25 +21,7 @@ const Home = () => {
   return (
     <Container className="rounded shadow">
       <Row className="bg-white flex-md-row h-100">
-        <Col xs="4" md="2" className="border-end px-0 bg-light flex-column h-100 d-flex">
-          <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-            <b>Каналы</b>
-            <Button size="sm" variant="outline-primary">
-              <Plus />
-            </Button>
-          </div>
-          <Nav className="flex-column nav-fill px-2 mb-3 overflow-auto h-100 d-block">
-            <Nav.Item>
-              <Button type="button" className="w-100 text-start rounded-0" variant="secondary">#general</Button>
-            </Nav.Item>
-            <Nav.Item>
-              <Button type="button" className="w-100 text-start rounded-0" variant="light">#random</Button>
-            </Nav.Item>
-            <Nav.Item>
-              <Button type="button" className="w-100 text-start rounded-0" variant="light">#fdsfd</Button>
-            </Nav.Item>
-          </Nav>
-        </Col>
+        <Channels />
         <Col className="p-0 h-100">
           <div className="d-flex flex-column h-100">
             <div className="bg-light mb-4 p-3 shadow-sm small">
