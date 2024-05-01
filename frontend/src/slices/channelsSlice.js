ef6c2065b0d0db8a327f3b41371671c3901d18e3
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-01 22:19:24 +0300

    added headers to getChannel

--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -1,9 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import routes from '../routes';
+import useSetHeaders from '../hooks';
 
 export const channelsApi = createApi({
   reducerPath: 'channels',
-  baseQuery: fetchBaseQuery({ baseUrl: routes.channels() }),
+  baseQuery: fetchBaseQuery(
+    { baseUrl: routes.channels(), prepareHeaders: useSetHeaders },
+  ),
   endpoints: (builder) => ({
     getChannels: builder.query({
       query: () => '',
