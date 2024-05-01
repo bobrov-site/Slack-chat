51c1a3c5118bcb7210d0611420f0e8bd74ab61c9
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-01 22:03:29 +0300

    remove rootReducer from app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { rootReducer } from './slices';
+import store from './slices';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => {
-  const store = configureStore({
-    reducer: rootReducer,
-  });
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
