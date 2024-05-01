5ed64dc1b47a18b38ac96e807571ceafce537214
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-01 22:31:48 +0300

    show channels

--- frontend/src/components/channels/Channels.jsx
+++ frontend/src/components/channels/Channels.jsx
@@ -2,15 +2,10 @@ import { Plus } from 'react-bootstrap-icons';
 import Nav from 'react-bootstrap/Nav';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import { useEffect } from 'react';
 import { useGetChannelsQuery } from '../../slices/channelsSlice';
 
 const Channels = () => {
-  const data = useGetChannelsQuery();
-  console.log(data);
-  useEffect(() => {
-    console.log('hello');
-  });
+  const { data: channels = [] } = useGetChannelsQuery();
   return (
     <Col xs="4" md="2" className="border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
@@ -20,15 +15,11 @@ const Channels = () => {
         </Button>
       </div>
       <Nav className="flex-column nav-fill px-2 mb-3 overflow-auto h-100 d-block">
-        <Nav.Item>
-          <Button type="button" className="w-100 text-start rounded-0" variant="secondary">#general</Button>
-        </Nav.Item>
-        <Nav.Item>
-          <Button type="button" className="w-100 text-start rounded-0" variant="light">#random</Button>
-        </Nav.Item>
-        <Nav.Item>
-          <Button type="button" className="w-100 text-start rounded-0" variant="light">#fdsfd</Button>
-        </Nav.Item>
+        {channels.map((channel) => (
+          <Nav.Item key={channel.id}>
+            <Button type="button" className="w-100 text-start rounded-0" variant="light">{`#${channel.name}`}</Button>
+          </Nav.Item>
+        ))}
       </Nav>
     </Col>
   );
