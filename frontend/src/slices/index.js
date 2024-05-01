cd18319e6e4120697b3a870ad8b8491dbae4a706
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-01 22:54:25 +0300

    created messages slice

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice.js';
 import { channelsApi } from './channelsSlice.js';
+import { messagesApi } from './messagesSlice.js';
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     [channelsApi.reducerPath]: channelsApi.reducer,
+    [messagesApi.reducerPath]: messagesApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(channelsApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(channelsApi.middleware, messagesApi.middleware),
 });
---
+++ frontend/src/slices/messagesSlice.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import routes from '../routes';
+import useSetHeaders from '../hooks';
+
+export const messagesApi = createApi({
+  reducerPath: 'messages',
+  baseQuery: fetchBaseQuery(
+    { baseUrl: routes.messages(), prepareHeaders: useSetHeaders },
+  ),
+  endpoints: (builder) => ({
+    getMessages: builder.query({
+      query: () => '',
+    }),
+  }),
+});
+
+export const {
+  useGetMessagesQuery,
+} = messagesApi;
