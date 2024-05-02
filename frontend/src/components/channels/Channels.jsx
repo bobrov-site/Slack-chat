ec6ba2fa6c64ea33bb65bfa9c87f05b2be319963
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-02 19:20:25 +0300

    change button style to active channel

--- frontend/src/components/channels/Channels.jsx
+++ frontend/src/components/channels/Channels.jsx
@@ -2,10 +2,13 @@ import { Plus } from 'react-bootstrap-icons';
 import Nav from 'react-bootstrap/Nav';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import { useSelector } from 'react-redux';
 import { useGetChannelsQuery } from '../../slices/channelsSlice';
 
 const Channels = () => {
   const { data: channels = [] } = useGetChannelsQuery();
+  const currentChannelId = useSelector((state) => state.app.currentChannelId);
+  const getVariantButton = (channel) => (channel.id === currentChannelId ? 'secondary' : 'light');
   return (
     <Col xs="4" md="2" className="border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
@@ -17,7 +20,7 @@ const Channels = () => {
       <Nav className="flex-column nav-fill px-2 mb-3 overflow-auto h-100 d-block">
         {channels.map((channel) => (
           <Nav.Item key={channel.id}>
-            <Button type="button" className="w-100 text-start rounded-0" variant="light">{`#${channel.name}`}</Button>
+            <Button type="button" className="w-100 text-start rounded-0" variant={getVariantButton(channel)}>{`#${channel.name}`}</Button>
           </Nav.Item>
         ))}
       </Nav>
