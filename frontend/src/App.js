233b9904b63c22d2327439b7959a662bdbcdee30
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-02 23:06:38 +0300

    added socket

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,31 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { socket } from './socket';
 import store from './slices';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    socket.on('newMessage', (payload) => {
+      console.log('connected');
+      console.log(payload);
+    });
+  });
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 export default App;
