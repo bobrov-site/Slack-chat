c5123d22bb4b58586785bcba46ef370fcadf9031
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-05 23:18:09 +0300

    added h-100

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import Init from './Init';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const html = document.getElementsByTagName('html');
+const body = document.getElementsByTagName('body');
+html[0].classList.add('h-100');
+body[0].classList.add('h-100');
+document.getElementById('root').classList.add('h-100');
 root.render(
   <React.StrictMode>
     <Init />
