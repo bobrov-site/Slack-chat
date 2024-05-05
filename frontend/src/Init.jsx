cd88d723e924c879dd265b8ba76bad11e1050112
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-05 23:18:01 +0300

    renamed to jsx

--- frontend/src/Init.jsx
+++ frontend/src/Init.jsx
@@ -1,10 +1,14 @@
 import { Provider } from 'react-redux';
 import store from './slices';
 import App from './App';
+import Header from './components/Header';
 
 const Init = () => (
   <Provider store={store}>
-    <App />
+    <div className="h-100 d-flex flex-column justify-content-between">
+      <Header />
+      <App />
+    </div>
   </Provider>
 );
 
