462f95367d70b5b7dc5b654b3e6660ef1476db25
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-05 23:18:29 +0300

    added h-100 to container

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   }, [token, navigate]);
   return (
     <>
-      <Container className="rounded shadow">
+      <Container className="rounded shadow h-100 mb-2">
         <Row className="bg-white flex-md-row h-100">
           <Channels />
           <Messages />
