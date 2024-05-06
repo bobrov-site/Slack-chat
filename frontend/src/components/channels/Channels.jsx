904645ef79f102bffc882963bff78afd23ebc17e
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-06 20:12:03 +0300

    remove messages and channel

--- frontend/src/components/channels/Channels.jsx
+++ frontend/src/components/channels/Channels.jsx
@@ -3,16 +3,21 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Modal from 'react-bootstrap/Modal';
+import { toast } from 'react-toastify';
 import { useSelector, useDispatch } from 'react-redux';
-import { useGetChannelsQuery } from '../../slices/channelsSlice';
-import { changeChannel } from '../../slices/appSlice';
-import { useGetMessagesQuery } from '../../slices/messagesSlice';
+import { useGetChannelsQuery, useRemoveChannelMutation } from '../../slices/channelsSlice';
+import { changeChannel, setShowModal } from '../../slices/appSlice';
+import { useGetMessagesQuery, useRemoveMessageMutation } from '../../slices/messagesSlice';
 import NewChannel from './NewChannel';
 
 const Channels = () => {
   const dispatch = useDispatch();
-  const { data: channels = [] } = useGetChannelsQuery();
-  const { refetch: refetchMessages } = useGetMessagesQuery();
+  const { data: channels = [], refetch } = useGetChannelsQuery();
+  const showModal = useSelector((state) => state.app.showModal);
+  const [removeChannel] = useRemoveChannelMutation();
+  const { data: messages = [], refetch: refetchMessages } = useGetMessagesQuery();
+  const [removeMessage] = useRemoveMessageMutation();
   const currentChannelId = useSelector((state) => state.app.currentChannelId);
   const getVariantButton = (channel) => (channel.id === currentChannelId ? 'secondary' : 'light');
   const switchChannel = (channel) => {
@@ -21,6 +26,28 @@ const Channels = () => {
       dispatch(changeChannel({ id, name }));
       refetchMessages();
     }
+  };
+  const handleShowModal = () => {
+    dispatch(setShowModal('delete-channel'));
+  };
+
+  const handleCloseModal = () => {
+    dispatch(setShowModal(''));
+  };
+  const deleteChannel = async (id) => {
+    const filtredMessages = messages.filter((message) => message.channelId === id);
+    if (filtredMessages.length > 0) {
+      const promises = filtredMessages.map(async (message) => {
+        await removeMessage(message.id);
+      });
+      await Promise.all(promises);
+      refetchMessages();
+    }
+    await removeChannel(id);
+    toast.success('Канал добавлен');
+    refetch();
+    handleCloseModal();
+    dispatch(changeChannel({ id: '1', name: 'general' }));
   };
   return (
     <Col xs="4" md="2" className="border-end px-0 bg-light flex-column h-100 d-flex">
@@ -38,7 +65,7 @@ const Channels = () => {
                 <Dropdown.Toggle className="text-end" split variant={getVariantButton(channel)} id={`dropdown-split-button${channel.id}`} />
 
                 <Dropdown.Menu>
-                  <Dropdown.Item>Удалить</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleShowModal()}>Удалить</Dropdown.Item>
                   <Dropdown.Item>Переименовать</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
@@ -52,6 +79,18 @@ const Channels = () => {
                 {channel.name}
               </Button>
             )}
+            <Modal show={showModal === 'delete-channel'} onHide={handleCloseModal}>
+              <Modal.Header closeButton>
+                <Modal.Title>Удалить канал</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <p>Вы действительно хотите удалить канал?</p>
+                <div className="d-flex justify-content-end mt-2">
+                  <Button type="button" variant="secondary" onClick={handleCloseModal} className="me-2">Отменить</Button>
+                  <Button type="button" variant="danger" onClick={() => deleteChannel(channel.id)}>Удалить</Button>
+                </div>
+              </Modal.Body>
+            </Modal>
           </Nav.Item>
         ))}
       </Nav>
