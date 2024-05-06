cc42e3384870a631622af5f82c2f796ac10ca4b3
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-06 18:16:19 +0300

    remove exit button when not auth

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,32 +1,35 @@
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/esm/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUserData } from '../slices/authSlice';
 
-const Header = () => (
+const Header = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  const logOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('nickname');
+    dispatch(setUserData({ nickname: '', token: null }));
+  };
+  return (
     <Navbar expand="lg" className="bg-body-tertiary mb-2">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Slack Chat</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ms-auto">
+            {auth.token ? (
+              <Button onClick={() => logOut()} variant="primary">Выйти</Button>
+            ) : (
+              ''
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-)
+  );
+};
 
 export default Header;
