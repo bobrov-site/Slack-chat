4e399a838bdd17425f393a6804860a67e74fc943
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-07 20:53:57 +0300

    added apiPath

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,7 @@
 const apiPath = 'api/v1';
 
 export default {
+  apiPath,
   signup: () => [apiPath, 'signup'].join('/'),
   login: () => [apiPath, 'login'].join('/'),
   messages: () => [apiPath, 'messages'].join('/'),
