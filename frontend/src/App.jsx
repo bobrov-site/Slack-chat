0f455fd6655de77e83306fa6089aacf8ee34a2c9
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-08 22:42:13 +0300

    added test event

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+import { Provider, ErrorBoundary } from '@rollbar/react'; // Provider imports 'rollbar'
 import Login from './pages/Login';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
@@ -13,6 +14,16 @@ import Signup from './pages/Signup';
 import Header from './components/Header';
 
 const App = () => {
+  const rollbarConfig = {
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    environment: 'production',
+  };
+  const TestError = () => {
+    const a = null;
+    return a.hello();
+  }
   const { refetch } = useGetMessagesQuery();
   const dispatch = useDispatch();
   useEffect(() => {
@@ -26,16 +37,21 @@ const App = () => {
     };
   }, [refetch, dispatch]);
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-      <ToastContainer />
-    </BrowserRouter>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <TestError />
+          <Header />
+          <Routes>
+            <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+          <ToastContainer />
+        </BrowserRouter>
+      </ErrorBoundary>
+    </Provider>
   );
 };
 
