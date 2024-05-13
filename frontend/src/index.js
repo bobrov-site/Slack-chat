f9bf12426180d35b51030b1b59b515a06050c739
Daniil Bobrov <allodrmen@yandex.ru>
2024-05-13 19:32:51 +0300

    fixed to querySelector in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import Init from './Init';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const html = document.getElementsByTagName('html');
-const body = document.getElementsByTagName('body');
-html[0].classList.add('h-100');
-body[0].classList.add('h-100');
+const html = document.querySelector('html');
+const body = document.querySelector('body');
+html.classList.add('h-100');
+body.classList.add('h-100');
 document.getElementById('root').classList.add('h-100');
 root.render(
   <React.StrictMode>
